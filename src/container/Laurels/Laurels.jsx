4ba75c161b3a487304a7564e6441fc0463cf7224
OyasumiiAlex <b181010@unach.mx>
2024-06-17 19:00:00 -0600

    add(feature): review section
    version 1.0.5-alpha

---
+++ src/container/Laurels/Laurels.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import { SubHeading } from '../../components';
+import { images, data } from '../../constants';
+import './Laurels.css';
+
+const AwardCard = ({ award: { imgUrl, title, subtitle } }) => (
+  <div className="app__laurels_awards-card">
+    <img src={imgUrl} alt="awards" />
+    <div className="app__laurels_awards-card_content">
+      <p className="p__cormorant" style={{ color: '#FFF5F1' }}>{title}</p>
+      <p className="p__opensans">{subtitle}</p>
+    </div>
+  </div>
+);
+
+const Laurels = () => (
+  <div className="app__bg app__wrapper section__padding" id="awards">
+    <div className="app__wrapper_info">
+      <SubHeading title="Premio / reconocimiento" />
+      <h1 className="headtext__cormorant">Nuestras Reseñas</h1>
+
+      <div className="app__laurels_awards">
+        {data.awards.map((award) => <AwardCard award={award} key={award.title} />)}
+      </div>
+    </div>
+
+    <div className="app__wrapper_img">
+      <img src={images.review} alt="laurels_img" />
+    </div>
+  </div>
+);
+
+export default Laurels;
