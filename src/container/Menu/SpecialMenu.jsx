c840f68f4b26e78c91f7ae39273dfb4ce5257e54
OyasumiiAlex <b181010@unach.mx>
2024-06-18 16:24:07 -0600

    feature(add): api consumption in the menu section
    version 1.1.1-alpha

--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -1,10 +1,47 @@
-import React from 'react';
-
+import React, { useState, useEffect } from 'react';
+import axios from "axios";
 import { SubHeading, MenuItem } from '../../components';
-import { data, images } from '../../constants';
+import { images } from '../../constants';
 import './SpecialMenu.css';
 
-const SpecialMenu = () => (
+const SpecialMenu = () => {
+  /*Objetos*/
+  const [menuItems, setMenuItems] = useState({ ap: [], fu: [], po: [], be: [] });
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  /*Solicitud HTTP y filtrado de Datos
+  AP -> Aperitivo
+  FU -> Fuerte
+  PO -> Postres
+  BE -> Bebidas*/
+  useEffect(() => {
+    axios.get('http://localhost:8080/api/v1/restaurante') //<-- Api path
+      .then(response => {
+        const items = response.data;
+        const categorizedItems = {
+          ap: items.filter(item => item.categoria === 'AP'),
+          fu: items.filter(item => item.categoria === 'FU'),
+          po: items.filter(item => item.categoria === 'PO'),
+          be: items.filter(item => item.categoria === 'BE'),
+        };
+        setMenuItems(categorizedItems);
+        setLoading(false);
+      })
+      .catch(error => {
+        setError(error);
+        setLoading(false);
+      });
+  }, []);
+
+  //Advertencia
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Section Missing Error API: {error.message}</div>;
+  }
+  return(
   <div className="app__specialMenu flex__center section__padding" id="menu">
     <div className="app__specialMenu-title">
       <SubHeading title="Un menú a tu paladar" />
@@ -15,9 +52,13 @@ const SpecialMenu = () => (
       <div className="app__specialMenu-menu_wine  flex__center">
         <p className="app__specialMenu-menu_heading">Aperitivos & Fuertes</p>
         <div className="app__specialMenu_menu_items">
-          {data.wines.map((wine, index) => (
-            <MenuItem key={wine.title + index} title={wine.title} price={wine.price} tags={wine.tags} />
+          {/*Mapeo de los datos de la API*/}
+          {menuItems.ap.map((item, index) => (
+            <MenuItem key={item.nombre + index} title={item.nombre} price={item.precio} />
           ))}
+          {menuItems.fu.map((item, index) => (
+              <MenuItem key={item.nombre + index} title={item.nombre} price={item.precio} tags={item.descripcion} />
+            ))}
         </div>
       </div>
 
@@ -28,9 +69,13 @@ const SpecialMenu = () => (
       <div className="app__specialMenu-menu_cocktails  flex__center">
         <p className="app__specialMenu-menu_heading">Postres & Bebidas</p>
         <div className="app__specialMenu_menu_items">
-          {data.cocktails.map((cocktail, index) => (
-            <MenuItem key={cocktail.title + index} title={cocktail.title} price={cocktail.price} tags={cocktail.tags} />
-          ))}
+            {/*Mapeo de los datos de la API*/}
+            {menuItems.po.map((item, index) => (
+              <MenuItem key={item.nombre + index} title={item.nombre} price={item.precio} tags={item.descripcion} />
+            ))}
+            {menuItems.be.map((item, index) => (
+              <MenuItem key={item.nombre + index} title={item.nombre} price={item.precio} tags={item.descripcion} />
+            ))}
         </div>
       </div>
     </div>
@@ -39,6 +84,6 @@ const SpecialMenu = () => (
       <button type="button" className="custom__button">Ver lista</button>
     </div>
   </div>
-);
+);}
 
 export default SpecialMenu;
